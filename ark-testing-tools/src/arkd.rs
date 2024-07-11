9ca6999cc20a398a1e52866c35c07e2549b97d43
Erik De Smedt <edesmedt@blockstream.com>
2024-07-11 10:29:31 +0200

    More stuff

--- ark-testing-tools/src/arkd.rs
+++ ark-testing-tools/src/arkd.rs
@@ -1,6 +1,19 @@
+use anyhow::Result;
+
+use bitcoin::Network;
 use std::path::PathBuf;
 
-pub struct Daemon {
-  datadir: PathBuf
+pub struct Arkd {
+	ark_exe: PathBuf,
+  datadir: PathBuf,
+	bitcoind_url: String,
+	bitcoind_cookie: String,
+	network: Network
 }
 
+impl Arkd {
+
+	fn create() {
+
+	}
+}
